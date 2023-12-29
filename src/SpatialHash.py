95437d7b82e1fac8beb932b31071d45250918faf
joshua <joshua@forevernine.com>
2023-12-29 14:46:48 +0800

    SpatialHash支持有宽度的物体

--- src/SpatialHash.py
+++ src/SpatialHash.py
@@ -1,54 +1,73 @@
-class SpatialHash:
-    def __init__(self, cell_size, width, height):
-        self.cell_size = cell_size  # size of each cell in the grid
+class SpatialEntity:
+    def __init__(self, x, y, width, height, data):
+        self.x = x
+        self.y = y
         self.width = width
         self.height = height
-        self.buckets = {}  # store data in a dict based on grid location
+        self.data = data
 
-    def _hash(self, x, y):
-        # Compute the hash value based on the cell size
-        return (int(x // self.cell_size), int(y // self.cell_size))
+    def get_bounds(self):
+        return (self.x, self.y, self.x + self.width, self.y + self.height)
 
-    def insert(self, x, y, data):
-        # Insert the data into the correct bucket based on its position
-        bucket_key = self._hash(x, y)
-        if bucket_key not in self.buckets:
-            self.buckets[bucket_key] = {}
-        # Use a tuple of (x, y) as the key for quick lookup
-        self.buckets[bucket_key][(x, y)] = data
+    def intersects(self, other):
+        return not (self.x > other.x + other.width or
+                    self.x + self.width < other.x or
+                    self.y > other.y + other.height or
+                    self.y + self.height < other.y)
 
-    def query_range(self, x, y, width, height):
-        # Query data within a given range
+class SpatialHash:
+    def __init__(self, cell_size, width, height):
+        self.cell_size = cell_size
+        self.width = width
+        self.height = height
+        self.buckets = {}
+
+    def _hash(self, entity):
+        bounds = entity.get_bounds()
+        return (
+            int(bounds[0] // self.cell_size),
+            int(bounds[1] // self.cell_size),
+            int(bounds[2] // self.cell_size),
+            int(bounds[3] // self.cell_size)
+        )
+
+    def insert(self, entity):
+        bucket_keys = self._hash(entity)
+        for i in range(bucket_keys[0], bucket_keys[2] + 1):
+            for j in range(bucket_keys[1], bucket_keys[3] + 1):
+                bucket_key = (i, j)
+                if bucket_key not in self.buckets:
+                    self.buckets[bucket_key] = {}
+                self.buckets[bucket_key][(entity.x, entity.y, entity.width, entity.height)] = entity.data
+
+    def query_range(self, query_entity):
         results = []
-        # Determine the range of cells that the query box intersects
-        min_key = self._hash(x, y)
-        max_key = self._hash(x + width, y + height)
-        
-        # Iterate over the range of relevant cells
-        for i in range(min_key[0], max_key[0] + 1):
-            for j in range(min_key[1], max_key[1] + 1):
+        bucket_keys = self._hash(query_entity)
+        for i in range(bucket_keys[0], bucket_keys[2] + 1):
+            for j in range(bucket_keys[1], bucket_keys[3] + 1):
                 bucket_key = (i, j)
                 if bucket_key in self.buckets:
-                    # Check each item in the bucket to see if it falls within the query range
-                    for (item_x, item_y), item_data in self.buckets[bucket_key].items():
-                        if (x <= item_x < x + width) and (y <= item_y < y + height):
-                            results.append(item_data)
+                    for (item_x, item_y, item_w, item_h), item_data in self.buckets[bucket_key].items():
+                        item_entity = SpatialEntity(item_x, item_y, item_w, item_h, item_data)
+                        if query_entity.intersects(item_entity):
+                            results.append(item_entity.data)
         return results
 
-    def remove(self, x, y, data):
-        # 根据其位置从正确的存储桶中移除数据
-        bucket_key = self._hash(x, y)
-        if bucket_key in self.buckets:
-            # Use the tuple (x, y) to directly access the item
-            if (x, y) in self.buckets[bucket_key] and self.buckets[bucket_key][(x, y)] == data:
-                del self.buckets[bucket_key][(x, y)]  # 直接删除
-                if not self.buckets[bucket_key]:
-                    del self.buckets[bucket_key]  # 如果存储桶为空，则移除存储桶
-                return True
-        return False
+    def remove(self, entity):
+        bucket_keys = self._hash(entity)
+        removed = False
+        for i in range(bucket_keys[0], bucket_keys[2] + 1):
+            for j in range(bucket_keys[1], bucket_keys[3] + 1):
+                bucket_key = (i, j)
+                if bucket_key in self.buckets:
+                    obj_key = (entity.x, entity.y, entity.width, entity.height)
+                    if obj_key in self.buckets[bucket_key] and self.buckets[bucket_key][obj_key] == entity.data:
+                        del self.buckets[bucket_key][obj_key]
+                        removed = True
+                        if not self.buckets[bucket_key]:
+                            del self.buckets[bucket_key]
+        return removed
 
-    def move(self, old_x, old_y, new_x, new_y, data):
-        # 尝试从旧位置移除项目
-        if self.remove(old_x, old_y, data):
-            # 仅当移除成功时才在新位置插入项目
-            self.insert(new_x, new_y, data)
+    def move(self, old_entity, new_entity):
+        if self.remove(old_entity):
+            self.insert(new_entity)
--- src/test.py
+++ src/test.py
@@ -1,9 +1,9 @@
 import random
 import time
 from QuadTree import QuadTree
-from SpatialHash import SpatialHash
+from SpatialHash import SpatialHash, SpatialEntity
 
-def performance_test(spatial_index, num_insertions: int, num_queries: int, num_movements: int, num_removals: int, seed: int = 0):
+def performance_test1(spatial_index, num_insertions: int, num_queries: int, num_movements: int, num_removals: int, seed: int = 0):
     random.seed(seed)
     
     # Prepare a list to keep track of inserted points for later removal and moving
@@ -57,9 +57,68 @@ def performance_test(spatial_index, num_insertions: int, num_queries: int, num_m
     if query_results:
         print(f"Query 1 returned {len(query_results[0])} points")
         
-	
+
+def performance_test2(spatial_index, num_insertions: int, num_queries: int, num_movements: int, num_removals: int, seed: int = 0):
+    random.seed(seed)
+    
+    # Prepare a list to keep track of inserted entities for later removal and moving
+    inserted_entities = []
+
+    # Insert random entities and add them to the spatial index
+    start_time = time.time()
+    for _ in range(num_insertions):
+        x = random.uniform(0, 1000)
+        y = random.uniform(0, 1000)
+        w = random.uniform(1, 10)  # Assign a small width
+        h = random.uniform(1, 10)  # Assign a small height
+        data = f"Entity({x},{y})"
+        entity = SpatialEntity(x, y, w, h, data)
+        spatial_index.insert(entity)
+        inserted_entities.append(entity)
+    end_time = time.time()
+    print(f"Inserted {num_insertions} entities in {end_time - start_time} seconds")
+
+    # Perform rectangular range queries on the spatial index
+    query_results = []
+    start_time = time.time()
+    for _ in range(num_queries):
+        qx = random.uniform(0, 1000)
+        qy = random.uniform(0, 1000)
+        qwidth = random.uniform(1, 100)
+        qheight = random.uniform(1, 100)
+        query_entity = SpatialEntity(qx, qy, qwidth, qheight, None)
+        result = spatial_index.query_range(query_entity)
+        query_results.append(result)
+    end_time = time.time()
+    print(f"Performed {num_queries} rectangular range queries in {end_time - start_time} seconds")
+
+    # Randomly move inserted entities
+    start_time = time.time()
+    for _ in range(num_movements):
+        entity = random.choice(inserted_entities)
+        new_x = random.uniform(0, 1000)
+        new_y = random.uniform(0, 1000)
+        new_entity = SpatialEntity(new_x, new_y, entity.width, entity.height, entity.data)
+        spatial_index.move(entity, new_entity)
+    end_time = time.time()
+    print(f"Moved {num_movements} entities in {end_time - start_time} seconds")
+
+    # Randomly remove inserted entities
+    start_time = time.time()
+    for _ in range(num_removals):
+        if inserted_entities:
+            entity = inserted_entities.pop()
+            spatial_index.remove(entity)
+    end_time = time.time()
+    print(f"Removed {num_removals} entities in {end_time - start_time} seconds")
+
+    # Output the number of entities returned by the first query
+    if query_results:
+        print(f"Query 1 returned {len(query_results[0])} entities")
+
+
 print("QuadTree:")
-performance_test(QuadTree(0, 0, 1000, 1000, 10), num_insertions=10000, num_queries=10000, num_movements=10000, num_removals=10000)
+performance_test1(QuadTree(0, 0, 1000, 1000, 10), num_insertions=10000, num_queries=10000, num_movements=10000, num_removals=10000)
 
 print("\nSpatialHash:")
-performance_test(SpatialHash(10, 1000, 1000), num_insertions=10000, num_queries=10000, num_movements=10000, num_removals=10000)
+performance_test2(SpatialHash(10, 1000, 1000), num_insertions=10000, num_queries=10000, num_movements=10000, num_removals=10000)
